460af6d86145dd09857ae90928419c0160fa1a4d
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2025-05-30 14:23:09 +0300

    Project Created.

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Beyza Zengin on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
